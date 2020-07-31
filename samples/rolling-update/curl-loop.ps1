2fa6b6560e5d9687520909ea6b788b94c566eede
Tarique Memon <tmemon@chemcentre.wa.gov.au>
2020-07-31 16:39:42 +0800

    wip

--- samples/rolling-update/curl-loop.ps1
+++ samples/rolling-update/curl-loop.ps1
@@ -1,5 +1,5 @@
 For ($i=0; $i -le 100; $i++) {
-    $html = Invoke-WebRequest http://localhost 
+    $html = Invoke-WebRequest http://192.168.0.10
     $html.Content -split "[`r`n]" | select-string "<title>.*</title>"
     Start-Sleep -s 2
 }
